2243152382b7ad8e8e6ac19f4fa86dd742aef5f4
EtoAndruwa <timonina1909@gmail.com>
2023-02-24 17:24:37 +0300

    Factorial code

---
+++ tests/factorial.asm
@@ -0,0 +1,29 @@
+PUSH 10
+POP cx
+
+CALL factorial:
+OUT
+HLT
+
+
+factorial:
+JZ :1
+
+PUSH cx
+DEC cx 
+JZ :2
+PUSH cx
+MUL
+DEC cx
+CALL factorial:
+MUL
+RET
+
+:1
+PUSH 1
+RET
+
+:2
+PUSH 1
+MUL
+RET
